7a5a11b0c5cfcdfc9f8c27bbf6c2d41973cd2698
Fioon <135500399+Fioon@users.noreply.github.com>
2024-03-09 00:07:16 +0800

    Update ChartingState.hx

--- source/meta/state/charting/ChartingState.hx
+++ source/meta/state/charting/ChartingState.hx
@@ -522,10 +522,10 @@ class ChartingState extends MusicBeatState
 
 	private function generateBackground()
 	{
-		coolGrid = new FlxBackdrop(null, 1, 1, true, true, 1, 1);
-		coolGrid.loadGraphic(Paths.image('UI/forever/base/chart editor/grid'));
-		coolGrid.alpha = (32 / 255);
-		add(coolGrid);
+		//coolGrid = new FlxBackdrop(null, 1, 1, true, true, 1, 1);
+		//coolGrid.loadGraphic(Paths.image('UI/forever/base/chart editor/grid'));
+		//coolGrid.alpha = (32 / 255);
+		//add(coolGrid);
 
 		// gradient
 		coolGradient = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height,
